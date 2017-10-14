42cd8f90b6c828bc14f67e0c24be4a129eb7f79a
MartyVos <marty.vos@student.hu.nl>
2017-10-14 17:35:35 +0200

    pe3

---
+++ pe3_2.py
@@ -0,0 +1,5 @@
+l = input("Geef je leeftijd: ")
+p = input("Nederlands paspoort: ")
+
+if int(l) >= 18 and p == "ja" or p == "Ja":
+    print("Gefeliciteerd, je mag stemmen!")
---
+++ pe3_4.py
@@ -0,0 +1,4 @@
+l = ["maandag", "dinsdag", "woensdag"]
+
+for x in l:
+    print(x[0] + x[1])
